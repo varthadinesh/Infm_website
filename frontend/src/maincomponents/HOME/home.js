adb988c94bb552a42d642ab69a032d0bb2d377ae
info-coe <150328529+info-coe@users.noreply.github.com>
2024-05-10 17:41:51 +0530

    Update home.js

--- frontend/src/maincomponents/HOME/home.js
+++ frontend/src/maincomponents/HOME/home.js
@@ -1,24 +1,12 @@
-<<<<<<< Updated upstream
 import React from 'react'
+import Navigation from '../../reusablecomponents/navbar';
 
 export default function Home(props) {
     const data = props.product;
     console.log(data);
   return (
     <div>
-=======
-import React from 'react';
-import Navigation from '../../reusablecomponents/navbar';
-
-const Home = () => {
-    return (
-        <div>
             <Navigation/>
-        </div>
-    );
-};
->>>>>>> Stashed changes
-
     </div>
   )
 }
